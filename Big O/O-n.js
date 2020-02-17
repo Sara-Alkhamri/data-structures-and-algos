a611fc1c48dbec31df422a6455435eb90747519b
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-17 15:10:53 -0800

    Completed first Big O notation

--- Big O/O-n.js
+++ Big O/O-n.js
@@ -11,3 +11,13 @@ function findNemo(array) {
   }
   
   findNemo(large);
+
+  //number of operations increases by number of elements in the array.
+
+  //O(n) ----> linear time. The (n) can be anything and it means that the Big O depends on the number of inputs. So for the first nemo 
+  //array, the n will equal 1 and for the large array, it will be 1000.
+
+  //o(n) is the most common Big O notation 
+
+  //Big O focuses on how quickly our run time grows. And it is done by using the size of the input(element) which is called (n) and comparing
+  // it to the number of operations that increase.
