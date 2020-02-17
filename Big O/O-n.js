bbc9dff65d6127e3528ee7c92020ec224ee2e689
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-17 14:56:51 -0800

    Created o-n file

---
+++ Big O/O-n.js
@@ -0,0 +1,13 @@
+const nemo = ['nemo'];
+
+const large = new Array(1000).fill('nemo');
+
+function findNemo(array) {
+    for (let i = 0; i < array.length; i ++) {
+      if (array[i] === 'nemo') {
+        console.log('Found Nemo!')
+      }
+    }
+  }
+  
+  findNemo(large);
