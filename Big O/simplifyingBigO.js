d6e9f3aa64c9982ecb1b288588a2b9dab0bab17a
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-17 17:38:52 -0800

    Added explanation of the first rule of Big O

---
+++ Big O/simplifyingBigO.js
@@ -0,0 +1,14 @@
+//Big O Rule Book
+
+//Rule 1: Worst Case.
+
+//We always care about what is the worst case scenario becuase we can't assume thongs will go well i.e when running a for loop, we can't assume
+//the item we are looking for will be the first in the array.
+
+//So if we can't be certain what the input will be, we will assume the big O is O(n) or linear time.
+
+//Rule 2: Remove Constants.
+
+//Rule 3: Different terms for inputs.
+
+//Rule 4: Drp non dominants 
