e790e33be45916d22d3dc3020e64a0ba638f180c
Sara Alkhamri <s.alkhamri@gmail.com>
2020-04-12 15:56:14 -0700

    Static Vs Dynamic Arrays lesson completed

---
+++ Data Structures/StaticVsDynamicArrays.js
@@ -0,0 +1,8 @@
+//There are two types of arrays:
+//1. Static
+//fixed in size, meaning you need to specify the number of elements the array will hold ahead of time.
+
+
+//2. Dynamic
+//allow us to copy and build an array at a new location with more memory
+//you don't need to determin the size of the array ahead of time.
