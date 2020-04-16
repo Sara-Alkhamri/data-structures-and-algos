d7235b87009644c036a52b578e447e1123d24d6f
Sara Alkhamri <s.alkhamri@gmail.com>
2020-04-15 20:40:37 -0700

    Recursion intro completed

---
+++ Algorithms/RecursionIntro.js
@@ -0,0 +1,10 @@
+//A Recursion function is a fucntion that refers to itself.
+
+//Recursion is best used for tasks that have repeated sub tasks to do.
+
+//Recusrion is used in searching and sorting
+
+//Example:
+function inception() {
+    inception();
+}
