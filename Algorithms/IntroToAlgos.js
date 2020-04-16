8d6b4cb1690eb55618280ccc9506089743c1e394
Sara Alkhamri <s.alkhamri@gmail.com>
2020-04-15 20:28:14 -0700

    Into to Algos completed

---
+++ Algorithms/IntroToAlgos.js
@@ -0,0 +1,2 @@
+//Algorithms are functions. They are steps in a process we take to perform a desired action with computers.
+//Algorithms allow us to use Data Structures to perform actions on that data.
